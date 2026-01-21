eca93b461fda492693add2c2588f3cfc01ac3278
matheusbd-dev <matheusbezerradomingos@gmail.com>
2026-01-21 09:05:26 -0300

    Template header

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
-import './../styles/global.css'
+import "./../styles/global.css";
+import "./../styles/header.css";
+import "./../styles/body.css";
 
-export default function MyApp({ Component, pageProps }){
-    return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,39 @@ import Link from "next/link";
 
 function Home() {
   return (
-    <header>
-      <Link href="/sobre">
-        <as>Olá</as>
-      </Link>
-    </header>
+    <>
+      <header>
+        <nav>
+          <ul id="links">
+            <li>
+              <Link href="/">/</Link>
+              <Link href="/sobre">Projects</Link>
+            </li>
+          </ul>
+
+          <ul id="networks">
+            <li>
+              <a href="https://github.com/matheusb-dev" target="_blank">
+                git
+              </a>
+              <a href="https://www.linkedin.com/in/..." target="_blank">
+                linkedin
+              </a>
+              <a href="https://instagram.com/..." target="_blank">
+                insta
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <main>
+        <div>
+          <h2>Matheus Bezerra Domingos</h2>
+          <p>Software Engineer</p>
+        </div>
+      </main>
+    </>
   );
 }
 
